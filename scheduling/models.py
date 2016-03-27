1618709f43729826bcbbefd7b4abcae01a8ae2e4
Benjamin Mallin <b.m.mallin@gmail.com>
2016-03-27 17:15:35 -0400

    Fix name error in Instructor model.

--- scheduling/models.py
+++ scheduling/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.postgres.fields import ArrayField
 from datetime import date
 
+
 # Create your models here.
 
 
@@ -13,28 +14,11 @@ class Classroom(models.Model):
         return self.name
 
 
-#Will this table also be used for helpers? Or a different one?
-#If we use the same, maybe we should have a boolean saying if they need to specialize at all
-class Instructor(models.Model):
-    name = models.CharField(max_length=255)
-    agePreferences = models.IntegerField(default=0) #I'm assuming this would just be an age number
-    #that they wouldn't want to teach below... We need to ask Dawn how she wants to do it
-    availability = ArrayField (
-        models.IntegerField()
-    )
-    specialty = ArrayField (
-        models.ForeignKey(Class, on_delete=models.PROTECT) #Dawn was assuming specialty was just the classes they could teach
-    )
-
-    def __str__(self):
-        return self.name
-
-
 class Schedule(models.Model):
     year = models.IntegerField(default=date.today().year)
-    version = models.CharField(max_length=255) #I'm assuming the version is really
-    #just what they name it so it's different from the auto primary key
-    schedule = models.TextField() #to store the JSON
+    version = models.CharField(max_length=255)  # I'm assuming the version is really
+    # just what they name it so it's different from the auto primary key
+    schedule = models.TextField()  # to store the JSON
 
     def __str__(self):
         return self.version
@@ -42,9 +26,27 @@ class Schedule(models.Model):
 
 class Class(models.Model):
     name = models.CharField(max_length=255)
-    ageGroup = models.CharField(max_length=10) #Just a written '??-??'
-    roomRequirement = ArrayField (
-        models.ForeignKey(Classroom, on_delete=models.PROTECT) #Room requirements need to match the actual classrooms
+    age_group = models.CharField(max_length=10)  # Just a written '??-??'
+    roomRequirement = ArrayField(
+        models.ForeignKey(Classroom, on_delete=models.PROTECT)  # Room requirements need to match the actual classrooms
+    )
+
+    def __str__(self):
+        return self.name
+
+
+# Will this table also be used for helpers? Or a different one?
+# If we use the same, maybe we should have a boolean saying if they need to specialize at all
+class Instructor(models.Model):
+    name = models.CharField(max_length=255)
+    age_preferences = models.IntegerField(default=0)  # I'm assuming this would just be an age number
+    # that they wouldn't want to teach below... We need to ask Dawn how she wants to do it
+    availability = ArrayField(
+        models.IntegerField()
+    )
+    specialty = ArrayField(
+        models.ForeignKey(Class, on_delete=models.PROTECT)
+        # Dawn was assuming specialty was just the classes they could teach
     )
 
     def __str__(self):
@@ -53,8 +55,8 @@ class Class(models.Model):
 
 class Document(models.Model):
     name = models.CharField(max_length=255)
-    forClass = models.ForeignKey(Class, on_delete=models.PROTECT)
-    document = models.FileField(upload_to='uploads/%Y/%m/%d/') #I'm not sure if this is right
+    for_class = models.ForeignKey(Class, on_delete=models.PROTECT)
+    document = models.FileField(upload_to='uploads/%Y/%m/%d/')  # I'm not sure if this is right
 
     def __str__(self):
         return self.name
