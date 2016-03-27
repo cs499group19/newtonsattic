0a744c8108376d0172b6a4c671d54272e5b8d5d3
Benjamin Mallin <b.m.mallin@gmail.com>
2016-03-27 19:16:17 -0400

    Register models for admin panel.

--- scheduling/admin.py
+++ scheduling/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Classroom
+from .models import Schedule
+from .models import Class
+from .models import Instructor
+from .models import Document
+
 # Register your models here.
+
+admin.site.register(Classroom)
+admin.site.register(Schedule)
+admin.site.register(Class)
+admin.site.register(Instructor)
+admin.site.register(Document)
