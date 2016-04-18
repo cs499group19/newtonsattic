c0a0735382d0e65de8145341052f216da89ae15b
Benjamin Mallin <b.m.mallin@gmail.com>
2016-04-18 14:18:18 -0400

    Setup url redirects.

--- newtonsattic/urls.py
+++ newtonsattic/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls import include
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^', include('scheduling.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^scheduling/', include('scheduling.urls')),
     url('^', include('django.contrib.auth.urls'))
