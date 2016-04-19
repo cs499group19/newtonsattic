ccef6fe190fd847e0d2262724c1c4527da2f27f1
Benjamin Mallin <b.m.mallin@gmail.com>
2016-04-18 22:19:29 -0400

    Pass all needed data into template.

--- scheduling/views.py
+++ scheduling/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.apps import apps
+from django.core import serializers
+from django.conf import settings
 
 # Create your views here.
 
 
 @login_required(login_url='/login/')
 def index(request):
-    context = {}
+    models_to_get = [
+        'Class',
+        'Classroom',
+        'Instructor',
+        'Schedule'
+    ]
+
+    models = map(lambda name: apps.get_model('scheduling', name), models_to_get)
+
+    context = {'data': {}, 'DEBUG': settings.DEBUG}
+    for model in models:
+        context['data'][model.__name__] = serializers.serialize('json', model.objects.all())
 
     return render(request, 'scheduling/schedule.html', context)
