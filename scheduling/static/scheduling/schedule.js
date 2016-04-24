c2a7979600ae28fea95663aca887f873107b9435
Benjamin Mallin <b.m.mallin@gmail.com>
2016-04-24 15:59:14 -0400

    Clean up template. Generate cards based on database data.

--- scheduling/static/scheduling/schedule.js
+++ scheduling/static/scheduling/schedule.js
@@ -14,29 +14,24 @@ function parseDatabaseData(data)
 
 //All of the following functions use jQuery
 $(function() {
-//variables that store the data and the calendar weeks
-var $data = $( 'ul[id^=data]' ), $calendar = $( 'div[id^=week]' );
-//variable that sets up tabs
-var tabs = $( "#tabs" ).tabs();
-
 //Declares which ids are draggable and how they will be implemented
-$( "li" , $data ).draggable({
-      revert: "invalid", // when not dropped, the item will revert back to its initial position
-      containment: "calendar",
-      helper: "clone",
-      cursor: "move",
-});
+    $('.course').draggable({
+        revert: "invalid", // when not dropped, the item will revert back to its initial position
+        containment: "calendar",
+        helper: "clone",
+        cursor: "move"
+    });
 
 //Declares which ids are droppable and how they will be implemented
-$( 'div[id^=week]' ).droppable({
-      accept: "ul[id^=data] > li",
-      //activeClass: "ui-state-highlight",  ---highlight the droppable that is being dropped into
-      drop: function( event, ui ) {
-           //alert( "dropped" );  --popup in the browser for testing purposes
-           deleteGroup( ui.draggable , event.target );
+    $('.time').droppable({
+        accept: "ul[id^=data] > li",
+        //activeClass: "ui-state-highlight",  ---highlight the droppable that is being dropped into
+        drop: function (event, ui) {
+            //alert( "dropped" );  --popup in the browser for testing purposes
+            deleteGroup(ui.draggable, event.target);
 
-      }
-});
+        }
+    });
 
 //Function to delete the draggable from the list and add it to the droppable when dropped
 //Called from the previous function
@@ -60,3 +55,13 @@ function deleteGroup( $item, $week ) {
       });
 }
 });
+
+// function handleDragStart(e)
+// {
+//     this.style.opacity = '0.4';
+// }
+//
+// var courseBoxes = document.querySelectorAll('.course');
+// [].forEach.call(courseBoxes, function (courseBox) {
+//     courseBox.addEventListener('dragstart', handleDragStart, false);
+// });
