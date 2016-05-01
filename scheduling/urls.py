914e57b5f8212d0c0d63da33841f8416f3c0ba99
Benjamin Mallin <b.m.mallin@gmail.com>
2016-05-01 19:05:34 -0400

    Allow schedules to be saved with the save button on the scheduling page.

--- scheduling/urls.py
+++ scheduling/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^register/$', views.register_user, name='register_user'),
     url(r'^new/$', views.create_new_schedule, name='new_schedule'),
+    url(r'^save/$', views.save_schedule, name='save_schedule'),
     url(r'^(?P<schedule_id>\d+)$', views.edit_schedule, name='edit_schedule'),
     url(r'^availability/$', views.instructor_availability, name='availability')
 ]
