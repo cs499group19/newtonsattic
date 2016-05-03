741982eb808d4741cdf236acc5b36a48482818ab
Suzie Hoover <Suzie@Gregorys-MacBook-Air.local>
2016-05-03 00:27:34 -0400

    The schedule is loaded correctly based on the previous save. There are now confirm boxes when trying to schedule and instructor who is already schduled at that time or scheduling a classroom that is already scheduled at that time.

--- scheduling/static/scheduling/schedule.js
+++ scheduling/static/scheduling/schedule.js
@@ -1,10 +1,39 @@
 // This file contains the javascript that is necessary for the
 //   scheduling.html page
 
+// Global variable that will store which instructors and classrooms are
+//    available when in order to determine conflicts to warn about
+var conflicts = {
+    1: { m: { instructor: [], classroom:[] },
+         a: { instructor: [], classroom:[] } },
+    2: { m: { instructor: [], classroom:[] },
+         a: { instructor: [], classroom:[] } },
+    3: { m: { instructor: [], classroom:[] },
+         a: { instructor: [], classroom:[] } },
+    4: { m: { instructor: [], classroom:[] },
+         a: { instructor: [], classroom:[] } },
+    5: { m: { instructor: [], classroom:[] },
+         a: { instructor: [], classroom:[] } },
+    6: { m: { instructor: [], classroom:[] },
+         a: { instructor: [], classroom:[] } },
+    7: { m: { instructor: [], classroom:[] },
+         a: { instructor: [], classroom:[] } },
+    8: { m: { instructor: [], classroom:[] },
+         a: { instructor: [], classroom:[] } },
+    9: { m: { instructor: [], classroom:[] },
+         a: { instructor: [], classroom:[] } },
+    10: { m: { instructor: [], classroom:[] },
+         a: { instructor: [], classroom:[] } },
+    11: { m: { instructor: [], classroom:[] },
+         a: { instructor: [], classroom:[] } },
+    12: { m: { instructor: [], classroom:[] },
+         a: { instructor: [], classroom:[] } }
+}
+
 var Week = function () {
     return {
-        morning: [],
-        afternoon: []
+        m: [],
+        a: []
     };
 };
 
@@ -18,7 +47,6 @@ var Schedule = function () {
     return rv;
 };
 
-
 var Course = function (id, title, instructors, rooms) {
     var rv = {};
 
@@ -30,7 +58,6 @@ var Course = function (id, title, instructors, rooms) {
     return rv;
 };
 
-
 var Instructor = function (id, name, availabilities, specialties) {
     var rv = {};
 
@@ -41,7 +68,6 @@ var Instructor = function (id, name, availabilities, specialties) {
 
     return rv;
 };
-
 
 var Classroom = function (id, name, capacity) {
     var rv = {};
@@ -78,30 +104,36 @@ var createDraggable = function (course, age, instructor, classroom) {
 // Function to generate the draggables based on what instructor, class, classroom, age group
 //   combinations are available.
 function generateAvailable(data, schedule, week, time) {
-    var available = [];
-    //var t = time.charAt(0).toLowerCase();
+    /*var t;
+    if (time == 'm') { t = "morning"; }
+    else if (time == 'a') { t = "afternoon"; }*/
+
     if (data[week] == null) {
-        return available;
+        return;
     }
     if (data[week][time] == null) {
-        return available;
+        return;
     }
     for (var pair in data[week][time]) {
         var course = data[week][time][pair][0];
         var instructor = data[week][time][pair][1];
 
-        /*var used = false;
-        if (schedule != "") {
-            for (var pair in schedule[week][time]) {
-                if ( pair[0]==course && pair[1]==instructor ) {
-                    used = true;
-                    break;
+        for (var room in course.room_requirement) {
+            var used = false;
+            if (schedule != null) {
+                for (var item in schedule['weeks'][week-1][time]) {
+                    var info = schedule['weeks'][week-1][time][item];
+                    if ( info.course==course.name
+                            && info.instructor==instructor.user.full_name
+                            && info.classroom==course.room_requirement[room].name
+                            && info.ageGroup==course.age_group) {
+                        used = true;
+                        break;
+                    }
                 }
             }
-        }*/
+            if (used == true) { continue; }
 
-
-        for (var room in course.room_requirement) {
             var courseItem = document.createElement('div');
             courseItem.setAttribute('class', 'box box-primary collapsed-box course');
 
@@ -158,41 +190,115 @@ function generateAvailable(data, schedule, week, time) {
     }
 }
 
-// Function to save the schedule as a JSON object.
+// Function to generate the draggables that are already in the schedule
+function generateSchedule(schedule, week, time) {
+    var t = time.charAt(0).toLowerCase();
+    if (schedule == null) {
+        return;
+    }
+    for (var item in schedule['weeks'][week-1][t]) {
+        var info = schedule['weeks'][week-1][t][item];
+
+        var courseItem = document.createElement('div');
+        courseItem.setAttribute('class', 'box box-primary collapsed-box course');
+
+        var div1 = document.createElement('div');
+        div1.setAttribute('class', 'box-header with-border');
+        courseItem.appendChild(div1);
+
+        var h3 = document.createElement('h3');
+        h3.setAttribute('class', 'box-title');
+        h3.setAttribute('id', 'courseName');
+        h3.innerHTML = info.course;
+        div1.appendChild(h3);
+
+        var div2 = document.createElement('div');
+        div2.setAttribute('class', 'box-tools pull-right');
+        div1.appendChild(div2);
+
+        var button = document.createElement('button');
+        button.setAttribute('class', 'btn btn-box-tool');
+        button.setAttribute('data-widget', 'collapse');
+        div2.appendChild(button);
+
+        var i = document.createElement('i');
+        i.setAttribute('class', 'fa fa-minus');
+        button.appendChild(i);
+
+        var div3 = document.createElement('div');
+        div3.setAttribute('class', 'box-body');
+        courseItem.appendChild(div3);
+
+        var ul = document.createElement('ul');
+        ul.setAttribute('id', 'course');
+        ul.setAttribute('style', 'list-style-type: circle');
+        div3.appendChild(ul);
+
+        var li1 = document.createElement('li');
+        li1.setAttribute('id', 'ageGroup');
+        li1.innerHTML = "Age Group: " + info.ageGroup;
+        ul.appendChild(li1);
+
+        var li2 = document.createElement('li');
+        li2.setAttribute('id', 'instructorName');
+        li2.innerHTML = "Instructor: " + info.instructor;
+        ul.appendChild(li2);
+
+        var li3 = document.createElement('li');
+        li3.setAttribute('id', 'classroomName');
+        li3.innerHTML = "Classroom: " + info.classroom;
+        ul.appendChild(li3);
+
+        var allCourses = document.getElementById('scheduled-'+week+time);
+        allCourses.appendChild(courseItem);
+
+        conflicts[week][t]['instructor'].push(info.instructor);
+        conflicts[week][t]['classroom'].push(info.classroom);
+    }
+}
+
+// Function that will parse the course, instructor, age group, classroom information
+var parseTextContent = function(allText) {
+    var obj = {};
+
+    var patt1 = /Group:\s/g;
+    patt1.exec(allText);
+    var patt2 = /Instructor:\s/g;
+    patt2.exec(allText);
+    var patt3 = /Classroom:\s/g;
+    patt3.exec(allText);
+
+    var titleEndIndex = allText.indexOf("Age");
+    var ageGroupStartIndex = patt1.lastIndex;
+    var ageGroupEndIndex = allText.indexOf("Instructor");
+    var instructorStartIndex = patt2.lastIndex;
+    var instructorEndIndex = allText.indexOf("Classroom");
+    var classroomStartIndex = patt3.lastIndex;
+
+
+    obj.course = allText.slice(0,titleEndIndex);
+    obj.instructor = allText.slice(instructorStartIndex, instructorEndIndex);
+    obj.ageGroup = allText.slice(ageGroupStartIndex, ageGroupEndIndex);
+    obj.classroom = allText.slice(classroomStartIndex);
+
+    return obj;
+}
+
+// Function to save the schedule as a JSON object
 var saveSchedule = function() {
     var schedule = Schedule();
 
     for (var i = 1; i <= 12; i++) {
         var weekm = document.getElementById('scheduled-'+i+'Morning').children;
-        for (var j = 0; j < weekm.length; j++) {
-            var obj = {};
-
+        for (var j = 1; j < weekm.length; j++) {
             var allText = weekm[j]['textContent'];
-            var patt1 = /Group:\s/g;
-            patt1.exec(allText);
-            var patt2 = /Instructor:\s/g;
-            patt2.exec(allText);
-            var patt3 = /Classroom:\s/g;
-            patt3.exec(allText);
+            var obj = parseTextContent(allText);
 
-            var titleEndIndex = allText.indexOf("Age");
-            var ageGroupStartIndex = patt1.lastIndex;
-            var ageGroupEndIndex = allText.indexOf("Instructor");
-            var instructorStartIndex = patt2.lastIndex;
-            var instructorEndIndex = allText.indexOf("Classroom");
-            var classroomStartIndex = patt3.lastIndex;
-
-
-            obj.course = allText.slice(0,titleEndIndex);
-            obj.instructor = allText.slice(instructorStartIndex, instructorEndIndex);
-            obj.ageGroup = allText.slice(ageGroupStartIndex, ageGroupEndIndex);
-            obj.classroom = allText.slice(classroomStartIndex);
-
-            schedule.weeks[i-1].morning.push(obj);
+            schedule.weeks[i-1].m.push(obj);
         }
 
         var weeka = document.getElementById('scheduled-'+i+'Afternoon').children;
-        for (var j = 0; j < weeka.length; j++) {
+        for (var j = 1; j < weeka.length; j++) {
             var obj = {};
 
             var allText = weeka[j]['textContent'];
@@ -216,17 +322,26 @@ var saveSchedule = function() {
             obj.ageGroup = allText.slice(ageGroupStartIndex, ageGroupEndIndex);
             obj.classroom = allText.slice(classroomStartIndex);
 
-            schedule.weeks[i-1].afternoon.push(obj);
+            schedule.weeks[i-1].a.push(obj);
         }
     }
 
     return JSON.stringify(schedule);
 }
 
-var confirmConflictingAction = function () {
+
+// Function to popup confirm box when there is an instructor conflict
+var confirmConflictingInstructor = function () {
     return confirm('The instructor for this class is ' +
-        'not available for this time of day. Are you ' +
-        'sure you want to do this?');
+        'already scheduled for a class at this time of day. ' +
+        'Are you sure you want to do this?');
+};
+
+// Function to popup confirm box when there is a classroom conflict
+var confirmConflictingClassroom = function () {
+    return confirm('The classroom for this class is ' +
+        'already scheduled for a class at this time of day. ' +
+        'Are you sure you want to do this?');
 };
 
 //All of the following functions use jQuery
@@ -262,15 +377,32 @@ $(function () {
          console.log($week);*/
         $item.fadeOut(function () {
             var $list = $("ul[class='timeOfDay']", $week);
-            var r = new RegExp('.+(Build).+');
-            var s = new RegExp('.+(Morning).+');
 
-            if (r.test($($item).context.innerText) && s.test($($week).context.innerHTML)) {
-                if (!confirmConflictingAction()) {
+            var allText = ($item).context.innerText;
+            var obj = parseTextContent(allText);
+
+            var weekTime = ($item).context.parentNode.id;
+            var index = weekTime.indexOf('-');
+            var w = weekTime.slice(index+1, (weekTime.length)-1);
+            var t = weekTime.slice((weekTime.length)-1);
+
+            if (conflicts[w][t]['instructor'].indexOf(obj.instructor) != -1) {
+                if (!confirmConflictingInstructor()) {
                     removeFromCalendar($item, $('.allCourses'));
                     return;
                 }
             }
+
+            if (conflicts[w][t]['classroom'].indexOf(obj.classroom) != -1) {
+                if (!confirmConflictingClassroom()) {
+                    removeFromCalendar($item, $('.allCourses'));
+                    return;
+                }
+            }
+
+            conflicts[w][t]['instructor'].push(obj.instructor);
+            conflicts[w][t]['classroom'].push(obj.classroom);
+
             $item.appendTo($list).fadeIn(function () {
                 /* --optional animation to modify later if we choose
                  $item
