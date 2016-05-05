ec9c2ebd11abce4ea05514bce2ecaabf56499462
Benjamin Mallin <b.m.mallin@gmail.com>
2016-05-05 12:41:35 -0400

    Fix error when rendering availability page.

--- scheduling/views.py
+++ scheduling/views.py
@@ -175,6 +175,15 @@ def instructor_availability(request):
         'weeks': range(1, 13),
     }
 
+    # load tab names
+    tab_settings = models.WeekHeadingsSettings.objects.first()
+
+    if tab_settings is None:
+        tab_settings = models.WeekHeadingsSettings()
+        tab_settings.save()
+
+    context['tab_settings'] = {k: tab_settings.name_for_week(k) for k in range(1, 13)}
+
     # if there is no instructor object attached to this user,
     # display a warning and return. We do not want to attempt to
     # load/store data in this case.
